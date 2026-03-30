fe59cb408baa8834661840e26248601f77a86238
Rahul Shrestha <rahulshrestha3132@gmail.com>
2026-03-30 21:25:35 +0545

    creating the server and adding prettier package

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,21 @@
-import express from 'express'
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
-app.listen()
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
+});
