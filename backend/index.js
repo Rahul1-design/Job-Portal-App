0b36133f95a1476be5aeea5f45ff27495a6c69b9
Rahul Shrestha <rahulshrestha3132@gmail.com>
2026-03-31 19:28:32 +0545

    adding dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import cookieParser from 'cookie-parser';
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config({});
 
 const app = express();
 
@@ -14,7 +16,7 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running at port ${PORT}`);
