9885a4e3dd13e0dad7cc7c9796642098b7f5c189
Rahul Shrestha <rahulshrestha3132@gmail.com>
2026-03-31 22:48:55 +0545

    connected with mongodb database and created user model and job model

---
+++ backend/models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose, { model, models, Schema } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    fullname: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phoneNumber: { type: Number, required: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['student', 'recruiter'], required: true }, //two options so we use enum
+    profile: {
+      bio: { type: String },
+      skills: [{ type: String }],
+      resume: { type: String }, //URL to resume file
+      resumeOriginalName: { type: String },
+      company: { type: mongoose.Schema.Types.ObjectId, ref: 'Company' },
+      profilePhoto: { type: String, default: '' },
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = models.User || model('User', userSchema);
