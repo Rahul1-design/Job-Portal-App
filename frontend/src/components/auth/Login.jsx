0110f5c4c6c13989b069c016cc125bbd4876f196
Rahul Shrestha <rahulshrestha3132@gmail.com>
2026-04-15 22:36:40 +0545

    Refactor code structure for improved readability and maintainability

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,7 +1,139 @@
-import React from "react";
+import React, { useState } from "react";
+import Navbar from "../shared/Navbar";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
+import { Button } from "../ui/button";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [open, setOpen] = useState(false);
+
+  const changeIcon = () => {
+    setOpen(!open);
+  };
+
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+    role: "",
+  });
+
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+    console.log({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log(input);
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <div className="flex items-center justify-center max-w-7xl mx-auto">
+        <form
+          onSubmit={submitHandler}
+          className="w-1/2 border border-gray-200 rounded-md p-4 my-10 shadow-md"
+        >
+          <h1 className="font-bold text-xl mb-5">Login</h1>
+          <div className="my-3 space-y-1">
+            <Label>Email</Label>
+            <Input
+              name="email"
+              onChange={changeEventHandler}
+              value={input.email}
+              type="text"
+              placeholder="abc@gmail.com"
+            />
+          </div>
+          <div className="my-3 space-y-1">
+            <Label>Password</Label>
+            <div className="flex relative items-center ">
+              <Input
+                name="password"
+                value={input.password}
+                onChange={changeEventHandler}
+                type={open ? "password" : "text"}
+                placeholder="Enter your password"
+              />
+              <span className="absolute right-3">
+                {open ? (
+                  <div>
+                    <img
+                      className="hover:cursor-pointer z-1"
+                      width={20}
+                      onClick={changeIcon}
+                      src="/eye-password-hide.svg"
+                      alt="Hide_Icon"
+                    />
+                  </div>
+                ) : (
+                  <div>
+                    <img
+                      className="hover:cursor-pointer z-2"
+                      onClick={changeIcon}
+                      width={20}
+                      src="/openeye.png"
+                      alt="Show_Icon"
+                    />
+                  </div>
+                )}
+              </span>
+            </div>
+          </div>
+          <div className="flex items-center justify-between">
+            <RadioGroup className="w-fit flex items-center gap-4">
+              <div className="flex items-center gap-2">
+                <Input
+                  name="role"
+                  onChange={changeEventHandler}
+                  type="radio"
+                  value="student"
+                  checked={input.role === "student"}
+                  className="cursor-pointer"
+                  id="r1"
+                />
+                <Label className={`cursor-pointer`} htmlFor="r1">
+                  Student
+                </Label>
+              </div>
+              <div className="flex items-center gap-2">
+                <Input
+                  type="radio"
+                  name="role"
+                  value="recruiter"
+                  onChange={changeEventHandler}
+                  checked={input.role === "recruiter"}
+                  className="cursor-pointer"
+                  id="r2"
+                />
+                <Label className={`cursor-pointer`} htmlFor="r2">
+                  Recruiter
+                </Label>
+              </div>
+            </RadioGroup>
+          </div>
+          <Button
+            type="submit"
+            className={`cursor-pointer w-full my-4 hover:bg-gray-700 p-5`}
+          >
+            Login
+          </Button>
+          <span className="flex justify-center">
+            Don't have an account?{" "}
+            <Link
+              className=" text-blue-600 hover:text-blue-800 hover:underline"
+              to="/signup"
+            >
+              Signup
+            </Link>
+          </span>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -1,41 +1,123 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../shared/Navbar";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
 import { Button } from "../ui/button";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
+  const [open, setOpen] = useState(false);
+
+  const changeIcon = () => {
+    setOpen(!open);
+  };
+  const [input, setInput] = useState({
+    fullname: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+    role: "",
+    file: "",
+  });
+
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+    // console.log({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const changeFileHandler = (e) => {
+    setInput({ ...input, file: e.target.files?.[0] });
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log(input);
+  };
   return (
     <div>
       <Navbar />
       <div className="flex items-center justify-center max-w-7xl mx-auto">
-        <form className="w-1/2 border border-gray-200 rounded-md p-4 my-10 shadow-md">
+        <form
+          onSubmit={submitHandler}
+          className="w-1/2 border border-gray-200 rounded-md p-4 my-10 shadow-md"
+        >
           <h1 className="font-bold text-xl mb-5">Sign Up</h1>
           <div className="my-3 space-y-1">
             <Label>Full Name</Label>
-            <Input type="text" placeholder="Enter your full name" />
+            <Input
+              value={input.fullname}
+              name="fullname"
+              onChange={changeEventHandler}
+              type="text"
+              placeholder="Enter your full name"
+            />
           </div>
           <div className="my-3 space-y-1">
             <Label>Email</Label>
-            <Input type="text" placeholder="abc@gmail.com" />
+            <Input
+              name="email"
+              value={input.email}
+              onChange={changeEventHandler}
+              type="text"
+              placeholder="abc@gmail.com"
+            />
           </div>
           <div className="my-3 space-y-1">
             <Label>Phone Number</Label>
-            <Input type="text" placeholder="Enter your phone number" />
+            <Input
+              value={input.phoneNumber}
+              name="phoneNumber"
+              onChange={changeEventHandler}
+              type="text"
+              placeholder="Enter your phone number"
+            />
           </div>
           <div className="my-3 space-y-1">
             <Label>Password</Label>
-            <Input type="password" placeholder="Enter your password" />
+            <div className="flex relative items-center ">
+              <Input
+                name="password"
+                value={input.password}
+                onChange={changeEventHandler}
+                type={open ? "password" : "text"}
+                placeholder="Enter your password"
+              />
+              <span className="absolute right-3">
+                {open ? (
+                  <div>
+                    <img
+                      className="hover:cursor-pointer z-1"
+                      width={20}
+                      onClick={changeIcon}
+                      src="/eye-password-hide.svg"
+                      alt="Hide_Icon"
+                    />
+                  </div>
+                ) : (
+                  <div>
+                    <img
+                      className="hover:cursor-pointer z-2"
+                      onClick={changeIcon}
+                      width={20}
+                      src="/openeye.png"
+                      alt="Show_Icon"
+                    />
+                  </div>
+                )}
+              </span>
+            </div>
           </div>
 
-          <div className="flex items-center justify-between">
+          <div className="flex items-center justify-between gap-5">
             <RadioGroup className="w-fit flex items-center gap-4 my-5">
               <div className="flex items-center gap-3">
                 <Input
                   type="radio"
                   name="role"
                   value="student"
+                  checked={input.role === "student"}
+                  onChange={changeEventHandler}
                   className="cursor-pointer"
                   id="r1"
                 />
@@ -43,11 +125,13 @@ const Signup = () => {
                   Student
                 </Label>
               </div>
-              <div className="flex items-center gap-3">
+              <div className="flex items-center gap-2">
                 <Input
                   type="radio"
                   name="role"
-                  value="Recruiter"
+                  value="recruiter"
+                  onChange={changeEventHandler}
+                  checked={input.role === "recruiter"}
                   className="cursor-pointer"
                   id="r2"
                 />
@@ -58,7 +142,12 @@ const Signup = () => {
             </RadioGroup>
             <div className="flex items-center gap-2">
               <Label>Profile</Label>
-              <Input type="file" accept="image/*" className="cursor-pointer" />
+              <Input
+                onChange={changeFileHandler}
+                type="file"
+                accept="image/*"
+                className="cursor-pointer"
+              />
             </div>
           </div>
           <Button
@@ -67,6 +156,15 @@ const Signup = () => {
           >
             Sign Up
           </Button>
+          <span className="flex justify-center">
+            Already have an account?{" "}
+            <Link
+              className=" text-blue-600 hover:text-blue-800 hover:underline"
+              to="/login"
+            >
+              Login
+            </Link>
+          </span>
         </form>
       </div>
     </div>
