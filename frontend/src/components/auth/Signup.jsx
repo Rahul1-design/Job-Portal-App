f207891ebc15410f819196b12b30388e0cc9c027
Rahul Shrestha <rahulshrestha3132@gmail.com>
2026-04-20 18:21:09 +0545

    integrating loading feature in singup button using redux

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -8,11 +8,16 @@ import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { USER_API_END_POINT } from "@/utils/constant";
 import { toast } from "sonner";
+import { useDispatch, useSelector } from "react-redux";
+import { setLoading } from "@/redux/authSlice";
+import { Loader2 } from "lucide-react";
 
 const Signup = () => {
   const [open, setOpen] = useState(true);
 
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { loading } = useSelector((store) => store.auth);
 
   const changeIcon = () => {
     setOpen(!open);
@@ -47,6 +52,7 @@ const Signup = () => {
       formData.append("file", input.file);
     }
     try {
+      dispatch(setLoading(true));
       const res = await axios.post(`${USER_API_END_POINT}/register`, formData, {
         headers: {
           "Content-Type": "multipart/form-data",
@@ -61,6 +67,8 @@ const Signup = () => {
     } catch (error) {
       console.log(error);
       toast.error(error.response.data.message);
+    } finally {
+      dispatch(setLoading(false));
     }
   };
   return (
@@ -179,12 +187,22 @@ const Signup = () => {
               />
             </div>
           </div>
-          <Button
-            type="submit"
-            className={`cursor-pointer w-full my-4 hover:bg-gray-700 p-5`}
-          >
-            Sign Up
-          </Button>
+          {loading ? (
+            <Button
+              className={`cursor-pointer w-full my-4 hover:bg-gray-700 p-5 `}
+            >
+              <Loader2 className="animate-spin mr-2 h-4 w-4" />
+              Please wait
+            </Button>
+          ) : (
+            <Button
+              type="submit"
+              className={`cursor-pointer w-full my-4 hover:bg-gray-700 p-5`}
+            >
+              Signup
+            </Button>
+          )}
+
           <span className="flex justify-center">
             Already have an account?{" "}
             <Link
