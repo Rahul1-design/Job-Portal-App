05f63dda7a5e8b94993a427db61e24972404ccef
Rahul Shrestha <rahulshrestha337@gmail.com>
2026-04-23 20:38:30 +0545

    feat: enhance FilterCard component with dynamic filtering options and UI elements

--- frontend/src/components/CategoryCarousel.jsx
+++ frontend/src/components/CategoryCarousel.jsx
@@ -19,7 +19,7 @@ const category = [
 const CategoryCarousel = () => {
   return (
     <div>
-      <Carousel className={`w-full max-w-xl mx-auto my-20`}>
+      <Carousel className={`w-full max-w-xl max-md:max-w-sm mx-auto my-20`}>
         <CarouselContent className={``}>
           {category.map((cat, index) => (
             <CarouselItem
--- frontend/src/components/FilterCard.jsx
+++ frontend/src/components/FilterCard.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
+import { Label } from "./ui/label";
+
+const filterData = [
+  {
+    filterType: "Location",
+    array: ["Kathmandu", "Biratnagar", "Pokhara", "Chitwan", "Bhaktapur"],
+  },
+  {
+    filterType: "Industry",
+    array: [
+      "Frontend Developer",
+      "Backend Developer",
+      "React Developer",
+      "MERN Stack Developer",
+      "Nextjs Developer",
+    ],
+  },
+  {
+    filterType: "Salary",
+    array: ["0-40k", "42k-1Lakh", "1lakh-3lakh", "3lakh-5lakh"],
+  },
+];
 
 const FilterCard = () => {
-  return <div>FilterCard</div>;
+  return (
+    <div className="w-full bg-white p-3 rounded-md">
+      <h2 className="text-xl font-bold">Filter Jobs</h2>
+      <hr className="mt-3" />
+      <RadioGroup>
+        {filterData.map((data, index) => (
+          <div>
+            <h2 className="font-bold text-lg">{data.filterType}</h2>
+            {data.array.map((item, index) => {
+              return (
+                <div className="flex gap-3 items-center my-2 space-x-2 ml-5">
+                  <RadioGroupItem value={item} />
+                  <Label>{item}</Label>
+                </div>
+              );
+            })}
+          </div>
+        ))}
+      </RadioGroup>
+    </div>
+  );
 };
 
 export default FilterCard;
