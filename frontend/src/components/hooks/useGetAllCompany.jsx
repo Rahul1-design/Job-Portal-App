13a7fc6fcf0702aea7b2953949b83d014fcd5c9f
Rahul1-design <rahulshrestha3132@gmail.com>
2026-05-09 20:06:23 +0545

    feat: add setCompanies reducer to companySlice; create useGetAllCompany hook for fetching all companies

---
+++ frontend/src/components/hooks/useGetAllCompany.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect } from "react";
+
+const useGetAllCompany = () => {
+  useEffect(() => {
+    const fetchAllCompany = async () => {};
+  }, []);
+};
+
+export default useGetAllCompany;
