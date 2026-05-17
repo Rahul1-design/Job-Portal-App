499c29a0a6867ba23961c881e83aa4f32fcf0c5f
Rahul1-design <rahulshrestha3132@gmail.com>
2026-05-17 20:52:18 +0545

    feat: add status update functionality for applicants; enhance resume display logic in ApplicantsTable

--- frontend/src/components/admin/ApplicantsTable.jsx
+++ frontend/src/components/admin/ApplicantsTable.jsx
@@ -11,10 +11,28 @@ import {
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { MoreHorizontal } from "lucide-react";
 import { useSelector } from "react-redux";
+import { toast } from "sonner";
+import axios from "axios";
+import { APPLICATION_API_END_POINT } from "@/utils/constant";
 
 const shortListingStatus = ["Accepted", "Rejected"];
 
 const ApplicantsTable = () => {
+  const statusHandler = async (status, id) => {
+    try {
+      const res = await axios.post(
+        `${APPLICATION_API_END_POINT}/status/${id}/update`,
+        { status },
+        { withCredentials: true },
+      );
+
+      if (res.data.success) {
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
   const { applicants } = useSelector((store) => store.application);
   return (
     <div>
@@ -41,13 +59,23 @@ const ApplicantsTable = () => {
                   <TableCell>{item?.applicant?.email}</TableCell>
                   <TableCell>{item?.applicant?.phoneNumber}</TableCell>
                   <TableCell>
-                    <a
-                      className="cursor-pointer italic text-blue-600"
-                      href={`https://docs.google.com/viewer?url=${encodeURIComponent(item?.applicant?.profile?.resume)}&embedded=true`}
-                      target="_blank"
-                    >
-                      {item?.applicant?.profile?.resumeOriginalName}
-                    </a>
+                    {item?.applicant?.profile?.resumeOriginalName ? (
+                      <a
+                        className="cursor-pointer italic text-blue-600"
+                        rel="noopener noreferrer"
+                        href={`https://docs.google.com/viewer?url=${encodeURIComponent(item?.applicant?.profile?.resume)}&embedded=true`}
+                        target="_blank"
+                      >
+                        {item?.applicant?.profile?.resumeOriginalName}
+                      </a>
+                    ) : (
+                      <span
+                        onClick={() => toast.error("No Resume")}
+                        className="cursor-pointer font-medium text-red-600"
+                      >
+                        NA
+                      </span>
+                    )}
                   </TableCell>
                   <TableCell>
                     {new Date(item?.createdAt).toLocaleDateString("en-Us", {
@@ -64,7 +92,11 @@ const ApplicantsTable = () => {
                       <PopoverContent className={`w-32`}>
                         {shortListingStatus.map((status, index) => {
                           return (
-                            <div className="cursor-pointer" key={index}>
+                            <div
+                              onClick={() => statusHandler(status, item._id)}
+                              className=" cursor-pointer"
+                              key={index}
+                            >
                               {status}
                             </div>
                           );
